c57cbe68cbae5b483d17161f48dfab6db307e2f6
wAlec1102 <walec1102@gmail.com>
2020-07-28 09:53:37 +0800

    feat:add google package

--- main/translator.py
+++ main/translator.py
@@ -5,5 +5,24 @@
     Author alec.wang
 """
 
+from googletrans import Translator
+import speech_recognition as sr
+
+
+def translate(text):
+    translator = Translator()
+    translation = translator.translate(text=text, src='en', dest='zh-cn')
+    print('translation:', text, '->', translation.text)
+
+
+def convert_audio_to_text():
+    recognizer = sr.Recognizer()
+    file_name = '../resources/audio/english.wav'
+    with sr.AudioFile(file_name) as source:
+        audio_data = recognizer.record(source)
+        return recognizer.recognize_google(audio_data, language='en', show_all=True)
+
+
 if __name__ == '__main__':
-    pass
+    results = convert_audio_to_text()['alternative']
+    translate(results[0]['transcript'])
